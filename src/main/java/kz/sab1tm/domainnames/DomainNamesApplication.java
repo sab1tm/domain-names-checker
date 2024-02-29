18a55e77047e632f95f3a1242cc6a2016e39c0d5
Sabit Murzaliev <s.murzaliyev@kus.kz>
2024-02-29 14:24:42 +0600

    first commit

---
+++ src/main/java/kz/sab1tm/domainnames/DomainNamesApplication.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.domainnames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainNamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainNamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sab1tm/domainnames/DomainNamesApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.domainnames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainNamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
